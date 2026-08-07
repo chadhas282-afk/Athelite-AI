f9750396b90d96df6fa408d3305390648cf90197
Sahil Chadha <Chadhas282@gmail.com>
2026-08-07 19:35:28 +0530

    feat: enhance App component layout and structure

--- src/App.jsx
+++ src/App.jsx
@@ -18,3 +18,29 @@ function App() {
   }, []);
 
   const handleSaveProfile = (newProfile) => {
+        setProfile(newProfile);
+    localStorage.setItem('athelite_profile', JSON.stringify(newProfile));
+  };
+
+  return (
+    <div className="min-h-screen bg-dark-base text-white font-sans flex flex-col">
+      <Header />
+      
+      <main className="flex-grow container mx-auto px-4 py-8 max-w-6xl">
+        <div className="grid grid-cols-1 lg:grid-cols-12 gap-8">
+          
+          <div className="lg:col-span-4">
+            <ProfileForm profile={profile} onSaveProfile={handleSaveProfile} />
+          </div>
+          
+          <div className="lg:col-span-8">
+            <Dashboard profile={profile} />
+          </div>
+
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
